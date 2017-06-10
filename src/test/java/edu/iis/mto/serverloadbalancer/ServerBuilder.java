b9a1cb939eeaa603d6016edd2b3b155efd87eb46
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-06-11 01:17:04 +0200

    refactoring

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerBuilder.java
@@ -1,5 +1,7 @@
 package edu.iis.mto.serverloadbalancer;
 
+import static edu.iis.mto.serverloadbalancer.ServerLoadBalancer.MAXIMUM_LOAD;
+
 /**
  * Created by Piotrek on 10.06.2017.
  */
@@ -15,7 +17,7 @@ public class ServerBuilder implements Builder<Server>{
     public Server build() {
         Server server = new Server(capacity);
         if (initialLoad > 0) {
-            int initialVmsSize = (int) (initialLoad / (double) capacity * 100.0d);
+            int initialVmsSize = (int) (initialLoad / (double) capacity * MAXIMUM_LOAD);
             Vm initialVm = VmBuilder.vm().ofSize(initialVmsSize).build();
             server.addVms(initialVm);
         }
