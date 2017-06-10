d30645feb4cbdcf7e071789cf69c67ce163cfc3e
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-06-11 00:20:12 +0200

    balancing one server with one slot capacity and one slot Vm fills server with The Vm

--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -25,6 +25,24 @@ public class ServerLoadBalancerTest {
 		assertThat(theServer, hasLoadPercentageOf(0.0d));
 	}
 
+	@Test
+	public void balancingOneServerWithOneSlotCapacity_andOneSlotVm_fillsServerWithTheVm(){
+		Server theServer = a(server().withCapacity(1));
+		Vm theVm = a(vm().ofSize(1));
+		balance(aListOfServersWith(theServer), aVmListWith(theVm));
+
+		assertThat(theServer, hasLoadPercentageOf(100.0d));
+		assertThat("server should contain the vm", theServer.contains(theVm));
+	}
+
+	private Vm[] aVmListWith(Vm... vms) {
+		return vms;
+	}
+
+	private VmBuilder vm() {
+		return new VmBuilder();
+	}
+
 	private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
@@ -41,5 +59,7 @@ public class ServerLoadBalancerTest {
 		return serverBuilder.build();
 	}
 
-
+	private Vm a(VmBuilder vmBuilder){
+		return vmBuilder.build();
+	}
 }
