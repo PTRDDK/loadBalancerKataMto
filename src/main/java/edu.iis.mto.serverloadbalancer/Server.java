0d7a82ba7aefcd80d96225801eafacf7471b6c19
Piotr Dudek <194936@edu.p.lodz.pl>
2017-06-11 17:46:43 +0200

    refactoring

--- src/main/java/edu.iis.mto.serverloadbalancer/Server.java
+++ src/main/java/edu.iis.mto.serverloadbalancer/Server.java
@@ -1,5 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * Created by Piotrek on 11.06.2017.
  */
@@ -7,6 +10,8 @@ public class Server {
     public static final double MAXIMUM_LOAD = 100.0d;
     public double currentLoadPercentage;
     public int capacity;
+
+    private List<Vm> vms = new ArrayList<Vm>();
 
     public boolean contains(Vm theVm) {
         return true;
@@ -17,6 +22,11 @@ public class Server {
     }
 
     public void addVms(Vm vm) {
-        currentLoadPercentage = (double)vm.size / capacity * MAXIMUM_LOAD;
+        currentLoadPercentage = (double)vm.size / (double)capacity * MAXIMUM_LOAD;
+        this.vms.add(vm);
+    }
+
+    public int countVms() {
+        return vms.size();
     }
 }
