5991641ea9c4b5cb54b7edaf6f32c601b5dcc8cd
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-06-12 12:09:35 +0200

    refactoring

--- src/main/java/edu/iis/mto/serverloadbalancer/Server.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/Server.java
@@ -1,6 +1,8 @@
 package edu.iis.mto.serverloadbalancer;
 
 
+import static edu.iis.mto.serverloadbalancer.ServerLoadBalancer.MAXIMUM_LOAD;
+
 public class Server {
 
 	public double currentLoadPecentage;
@@ -14,4 +16,7 @@ public class Server {
 		return true;
 	}
 
+    public void addVms(Vm vm) {
+        currentLoadPecentage = (double)vm.size / (double) capacity * MAXIMUM_LOAD;
+    }
 }
--- src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
+++ src/main/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancer.java
@@ -2,9 +2,11 @@ package edu.iis.mto.serverloadbalancer;
 
 public class ServerLoadBalancer {
 
+	public static final double MAXIMUM_LOAD = 100.0d;
+
 	public void balance(Server[] servers, Vm[] vms) {
 		if (vms.length > 0) {
-			servers[0].currentLoadPecentage = (double)vms[0].size / (double)servers[0].capacity * 100.0d;
+			servers[0].addVms(vms[0]);
 		}
 	}
 
