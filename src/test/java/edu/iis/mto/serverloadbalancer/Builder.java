54df3c2a38324f73305b2f4a2285fae86b41bd7d
Dudek.Piotr <194936@edu.p.lodz.pl>
2017-06-12 11:56:45 +0200

    refactoring

---
+++ src/test/java/edu/iis/mto/serverloadbalancer/Builder.java
@@ -0,0 +1,8 @@
+package edu.iis.mto.serverloadbalancer;
+
+/**
+ * Created by Piotrek on 12.06.2017.
+ */
+public interface Builder<T> {
+    T build();
+}
--- src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
+++ src/test/java/edu/iis/mto/serverloadbalancer/ServerLoadBalancerTest.java
@@ -2,6 +2,7 @@ package edu.iis.mto.serverloadbalancer;
 
 import static edu.iis.mto.serverloadbalancer.CurrentLoadPercentageMatcher.hasLoadPercentageOf;
 import static edu.iis.mto.serverloadbalancer.ServerBuilder.server;
+import static edu.iis.mto.serverloadbalancer.VmBuilder.vm;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.equalTo;
 
@@ -36,14 +37,6 @@ public class ServerLoadBalancerTest {
         return vms;
     }
 
-    private Vm a(VmBuilder builder) {
-        return builder.build();
-    }
-
-    private VmBuilder vm() {
-        return new VmBuilder();
-    }
-
     private void balance(Server[] servers, Vm[] vms) {
 		new ServerLoadBalancer().balance(servers, vms);
 	}
@@ -56,8 +49,7 @@ public class ServerLoadBalancerTest {
 		return new Server[] { server };
 	}
 
-	private Server a(ServerBuilder builder) {
-		return builder.build();
-	}
-
+	private <T> T a(Builder<T> builder){
+	    return builder.build();
+    }
 }
